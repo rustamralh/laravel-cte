d7b2e14b1e78552b3ae6113a0d97edcbf24d05be
Jatin Thapar <jatinthapar@icloud.com>
2025-12-26 11:25:50 +0530

    fix: update PHP version requirement to ^8.2 in composer.json

--- src/Connections/PostgresConnection.php
+++ src/Connections/PostgresConnection.php
@@ -3,8 +3,19 @@
 namespace Staudenmeir\LaravelCte\Connections;
 
 use Illuminate\Database\PostgresConnection as Base;
+use Staudenmeir\LaravelCte\Query\Grammars\PostgresGrammar as QueryGrammar;
 
 class PostgresConnection extends Base
 {
     use CreatesQueryBuilder;
+
+    /**
+     * Get the default query grammar instance.
+     *
+     * @return \Staudenmeir\LaravelCte\Query\Grammars\PostgresGrammar
+     */
+    protected function getDefaultQueryGrammar()
+    {
+        return new QueryGrammar($this);
+    }
 }
--- src/Query/Grammars/PostgresGrammar.php
+++ src/Query/Grammars/PostgresGrammar.php
@@ -8,4 +8,17 @@ use Staudenmeir\LaravelCte\Query\Grammars\Traits\CompilesPostgresExpressions;
 class PostgresGrammar extends Base implements ExpressionGrammar
 {
     use CompilesPostgresExpressions;
+
+    /**
+     * Wrap a table in keyword identifiers.
+     *
+     * @param  \Illuminate\Contracts\Database\Query\Expression|string  $table
+     * @param  string|null  $prefix
+     * @return string
+     */
+    public function wrapTable($table, $prefix = null)
+    {
+        // Laravel 12 fix: Handle NULL connection in tests (DatabaseTransactions edge case)
+        return parent::wrapTable($table, $prefix ?? ($this->connection ? null : ''));
+    }
 }
